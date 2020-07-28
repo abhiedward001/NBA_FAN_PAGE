0020b503a7da33554defbf3bb96dc60f7e145b3b
abhiedward001 <iam001abhi@gmail.com>
2020-07-28 22:02:43 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+     const  express=require("express");
+     const app=express();
+     const bodyparser=require("body-parser");
+
+     const seedDb = require("./seed")
+     const  mongoose=require("mongoose");
+
+     const passport = require("passport");
+     const loaclstrategy=require("passport-local");
+     const passportlocalmongoose=require("passport-local-mongoose");
+     var yelpcalm=require("./routes/yelpcalm");
+   
+     var User=require("./models/user");
+
+    
+// PASSPORT CONFIG
+
+app.use(require("express-session")({
+    secret:"Abhishek jaiswal is abhiedward001",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyparser.urlencoded({extended:true}));
+passport.use(new loaclstrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());   //for encoding session
+passport.deserializeUser(User.deserializeUser());  // for decoding session
+
+// ------------------------------------------------------------------------------
+     app.use("",yelpcalm);
+     app.use(express.static("public"));
+     app.use(bodyparser.urlencoded({extended:true}));
+     app.set("view engine", "ejs");
+    
+
+seedDb();
+
+mongoose.connect("mongodb+srv://abhiedward001:abhi@123@cluster0.akhwn.mongodb.net/nbafanpage?retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology: true }).then(()=>{
+    console.log("connected to dbi");
+}).catch((err)=>{
+    console.log("error",err);
+});
+
+app.listen(4000, ()=>{
+console.log("someone staretd the server");
+});
---
+++ routes/yelpcalm.js
@@ -0,0 +1,228 @@
+var  express=require("express"),
+     router=express(),
+     bodyparser=require("body-parser"),
+     mongoose=require("mongoose"),
+     passport = require("passport"),
+     loaclstrategy=require("passport-local"),
+     passportlocalmongoose=require("passport-local-mongoose"),
+     methodoverride=require("method-override");
+  
+    
+var Campground=require("../models/campground");
+var Comment=require("../models/comment");
+var User=require("../models/user");
+const { authorize } = require("passport");
+var router=express.Router();
+     
+
+router.use(function(req,res,next){
+    res.locals.currentUser=req.user;
+    next();
+});
+router.use(methodoverride("_method"));
+
+//----------------- Campground Routes--------------------
+
+router.get("/",function(req,res){
+    res.render("landing");
+});
+
+router.get("/campgrounds",isloggedin,(req,res)=>{
+    Campground.find({},function(err,allcampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("campground/index",{campground:allcampground,currentUser:req.user});
+        }
+    });
+
+  
+});
+router.post("/campgrounds",isloggedin,function(req,res){
+    var name=req.body.name;
+    var image=req.body.image;
+    var descp=req.body.description;
+    var author={
+        id:req.user._id,
+        username:req.user.username
+    }
+    var newcamp={name:name,image:image,description:descp,author:author};
+    Campground.create(newcamp,function(err,data){
+        if (err){
+            console.log(err);
+        }
+        else{
+            console.log(data);
+        }
+    })
+    res.redirect("/campgrounds");
+});
+router.get("/campgrounds/new",(req,res)=>{
+    res.render("campground/new");
+
+});
+// show route
+router.get("/campgrounds/:id",isloggedin,(req,res)=>{
+    // found the campground with provided id
+    Campground.findById(req.params.id).populate("comments").exec(function(err,foundcampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            // render the show template with campground
+          
+            res.render("campground/show",{campground:foundcampground});
+        }
+    });
+    
+});
+
+// edit route
+router.get("/campgrounds/:id/edit",isloggedin,(req,res)=>{
+    Campground.findById(req.params.id,function(err,foundcampground){
+        if(err){
+            console.log(err);
+           return  res.redirect("/");
+        }
+        else{
+            if(foundcampground.author.id.equals(req.user._id)){
+            res.render("campground/edit",{campground:foundcampground});
+            }
+            else{
+                res.send("Sorry! you dont have permission to do so.This post can only be updated by the author of this.");
+            }
+        }
+    });
+   
+});
+// update route
+router.put("/campgrounds/:id",isloggedin,(req,res)=>{
+    Campground.findByIdAndUpdate(req.params.id,req.body.camp,function(err,updateddata){
+        if(err){
+            console.log(err);
+            return res.redirect("/");
+        }
+        else{
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    })
+})
+
+// delete route
+router.delete("/campgrounds/:id",isloggedin,function(req,res){
+    Campground.findById(req.params.id,function(err,foundcampground){
+        if(err){
+            console.log(err);
+           return  res.redirect("/");
+        }
+        else{
+            if(foundcampground.author.id.equals(req.user._id)){
+                Campground.findByIdAndRemove(req.params.id,function(err){
+                    if(err){
+                        res.redirect("/");
+                    }
+                    else{
+                        res.redirect("/campgrounds");
+                    }
+                });
+            }
+            else{
+                res.send("Sorry! you dont have permission to do so.This post can only be updated by the author of this.");
+            }
+        }
+    });
+   
+});
+
+// ==========================================
+// ==========================================
+// comments routes
+router.get("/campgrounds/:id/comments/new",isloggedin,(req,res)=>{
+    Campground.findById(req.params.id,(err,campground)=>{
+        if(err){
+            res.send("the error is"+err);
+        }
+        else{
+            res.render("comment/new",{campground:campground,currentUser:req.user});
+        }
+    })
+});
+router.post("/campgrounds/:id/comments",isloggedin,(req,res)=>{
+Campground.findById(req.params.id,(err,campground)=>{
+    if(err){
+        console.log("the error is:"+err);
+    }
+    else{
+       
+        Comment.create(req.body.comment,(err,comment)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                comment.author.id=req.user._id;
+                comment.author.username=req.user.username;
+               comment.save();
+                campground.comments.push(comment);
+                campground.save();
+                res.redirect("/campgrounds/"+campground._id);
+            }
+        });
+    }
+});
+});
+
+// =====================AUTHENTICATION ROUTES==============================
+
+router.get("/signup",(req,res)=>{
+    res.render("auth/signup",{currentUser:req.user});
+});
+
+// sign up route
+
+router.post("/signup",(req,res)=>{
+   var newuser=new User({username:req.body.username});
+   User.register(newuser,req.body.password,function(err,user){
+       if(err){
+           console.log(err);
+           return res.render("auth/signup");
+       }
+       passport.authenticate("local")(req,res,function(){
+           res.redirect("/login");
+       });
+   });
+
+});
+
+
+// login route,
+router.get("/login",(req,res)=>{
+    res.render("auth/login",{currentUser:req.user});
+});
+
+// login logic
+router.post("/login",passport.authenticate("local",{
+    successRedirect:"/campgrounds",
+    failureRedirect:"/login"
+}),function(req,res){
+
+});
+
+// logout
+router.get("/logout",(req,res)=>{
+    req.logOut();
+    res.redirect("/");
+});
+
+// middleware
+ function isloggedin(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+
+
+module.exports = router;
